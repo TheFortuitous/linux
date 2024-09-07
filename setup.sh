32f37f109744ddd95cabd33759c66ce621a8aeaa
Aleksander <54500854+abuschno@users.noreply.github.com>
2024-09-07 21:19:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 # Check if the script is run as root or with sudo privileges
-if [ "$EUID" -ne 0 ]; then
-    echo "This script needs to be run as root or with sudo privileges."
+if [ "$(id -u)" -ne 0 ]; then
     echo "Re-running the script with sudo..."
     exec sudo bash "$0" "$@"
-    exit
+    exit 1
 fi
+
+# Get the original user who ran the script using sudo
+ORIGINAL_USER="${SUDO_USER:-$USER}"
+USER_HOME=$(eval echo ~$ORIGINAL_USER)
 
 # Function to check if a command exists
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
+
+# Check if Curl is installed
+if ! command_exists curl; then
+    echo "Curl is not installed. Installing Curl..."
+    apt update && apt install -y curl
+else
+    echo "curl is already installed."
+fi
 
 # Check if Git is installed
 if ! command_exists git; then
@@ -30,48 +41,52 @@ else
 fi
 
 # Install Oh My Zsh
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
+if [ ! -d "$USER_HOME/.oh-my-zsh" ]; then
     echo "Installing Oh My Zsh..."
-    sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sudo -u "$ORIGINAL_USER" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 else
     echo "Oh My Zsh is already installed."
 fi
 
 # Install Powerlevel10k theme
-if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+if [ ! -d "$USER_HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
     echo "Installing Powerlevel10k..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    sudo -u "$ORIGINAL_USER" git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$USER_HOME/.oh-my-zsh/custom/themes/powerlevel10k"
 else
     echo "Powerlevel10k is already installed."
 fi
 
 # Set Powerlevel10k as the theme in .zshrc
 echo "Setting Powerlevel10k as the theme in .zshrc..."
-sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+sudo -u "$ORIGINAL_USER" sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$USER_HOME/.zshrc"
 
 # Install zsh-autosuggestions plugin
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+if [ ! -d "$USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
     echo "Installing zsh-autosuggestions..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    sudo -u "$ORIGINAL_USER" git clone https://github.com/zsh-users/zsh-autosuggestions "$USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 else
     echo "zsh-autosuggestions is already installed."
 fi
 
 # Add zsh-autosuggestions to plugins in .zshrc if not already present
-if ! grep -q "zsh-autosuggestions" ~/.zshrc; then
+if ! grep -q "zsh-autosuggestions" "$USER_HOME/.zshrc"; then
     echo "Adding zsh-autosuggestions to plugins in .zshrc..."
-    sed -i 's/plugins=(/&zsh-autosuggestions /' ~/.zshrc
+    sudo -u "$ORIGINAL_USER" sed -i 's/plugins=(/&zsh-autosuggestions /' "$USER_HOME/.zshrc"
 fi
 
 # Download the Powerlevel10k configuration file (.p10k.zsh) from the given GitHub URL
 echo "Downloading Powerlevel10k configuration..."
-wget -O ~/.p10k.zsh https://raw.githubusercontent.com/abuschno/linux/master/.p10k.zsh
+sudo -u "$ORIGINAL_USER" wget -O "$USER_HOME/.p10k.zsh" https://raw.githubusercontent.com/abuschno/linux/master/.p10k.zsh
+
+# Download the zshrc configuration
+echo "Downloading zshrc configuration..."
+sudo -u "$ORIGINAL_USER" wget -O "$USER_HOME/.zshrc" https://raw.githubusercontent.com/abuschno/linux/master/zshrc
 
 # Ensure the .p10k.zsh is sourced in .zshrc
-if ! grep -q "source ~/.p10k.zsh" ~/.zshrc; then
-    echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
+if ! grep -q "source ~/.p10k.zsh" "$USER_HOME/.zshrc"; then
+    echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' | sudo -u "$ORIGINAL_USER" tee -a "$USER_HOME/.zshrc"
 fi
 
-# Reload Zsh to apply the changes
+# Reload Zsh to apply the changes for the original user
 echo "Reloading zsh..."
-exec zsh
+sudo -u "$ORIGINAL_USER" zsh
